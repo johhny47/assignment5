b388b0550a9c3e866abf76e3e93c0e3c92579ed8
johhny47 <mdjohnny063@gmail.com>
2024-09-24 15:24:01 +0600

    edit file name

--- js/blog.js
+++ js/blog.js
@@ -2,7 +2,7 @@
  const back = document.getElementById('home-btn')
   back.addEventListener('click',function(){
    
-   window.location.href='../index.html'
+   window.location.href='../home.html'
   console.log("hi")
 
 }); 
